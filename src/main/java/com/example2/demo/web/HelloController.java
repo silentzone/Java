a02be45b0cb986dfa041a3d2989bc3ac16c76abf
li-e580 <lioff@qq.com>
2018-07-12 22:09:07 +0800

    add JSP

---
+++ src/main/java/com/example2/demo/web/HelloController.java
@@ -0,0 +1,31 @@
+package com.example2.demo.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
+    }
+}
+
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloController {
+//
+//    @RequestMapping("/hello")
+//    public String hello() {
+//        return "Hello Spring Boot!";
+//    }
+//
+//}
